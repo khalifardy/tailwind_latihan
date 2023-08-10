27a304d1977aee49ecde60a094c1227fe16f3f0a
khalifardy <khalifardy.miqdarsah@gmail.com>
2023-08-11 04:39:49 +0700

    portfolio part1

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      letterSpacing:{
+        spasi_luas:'.5em',
+      }
+    },
+  },
+  plugins: [],
+}
+
